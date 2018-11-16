abce4b75b50df25a0ec8268b0243badcca919587
Faizan Mamji <Faizanmamji1@gmail.com>
2018-11-16 14:54:30 +0500

    Credentials update due to user issue.

--- src/test/java/TestngMethods/MainTestCaseFile.java
+++ src/test/java/TestngMethods/MainTestCaseFile.java
@@ -42,8 +42,8 @@ public class MainTestCaseFile extends MainDriverClass {
 	// Just to check for push..
 
 	public static WebDriver maindriver;
-	String Login_UserName = "arpatech1@connection.com";
-	String Login_Password = "Arp@tech5";
+	String Login_UserName = "gcarrasco@netflix.com";
+	String Login_Password = "Arp@tech1";
 	static final Logger logg = LogManager.getLogger(MainTestCaseFile.class);
 
 	@BeforeTest
